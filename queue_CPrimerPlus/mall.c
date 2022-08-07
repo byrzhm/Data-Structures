ec83a919bf2fb8d4e5ad4f84757837c2b4aa4d9e
byrzhm <zhongguoai87@gmail.com>
2022-08-07 18:31:47 +0800

    modified:   queue_CPrimerPlus/mall.c

---
+++ queue_CPrimerPlus/mall.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "queue.h"
+#define MIN_PER_HR 60.0
+
+bool newcustomer(double x);
+Item customertime(long whem);
+
+int main(void)
+{
+    
+}
